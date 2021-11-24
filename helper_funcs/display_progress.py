851783265801f0dc8c4fe2ffdeb10bac3624dd19
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-24 12:53:42 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -48,7 +48,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
             humanbytes(total),
             humanbytes(speed),
             # elapsed_time if elapsed_time != '' else "0 s",
-            estimated_total_time if estimated_total_time != '' else "0 s"│\n├@KicchaRequest\n╰──────〄"
+            estimated_total_time if estimated_total_time != '' else "0 s"│\n╰──────〄"
         )
         try:
             await message.edit(
