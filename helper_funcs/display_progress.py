7161c391e5c89233bfb9f66409f6a8df1d961e87
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-24 12:22:01 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -38,10 +38,10 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "╭────<b>ᴜᴘʟᴏᴀᴅɪɴɢ </b> {2}%\n".format(
+        progress = "╭────[{0}{1}] \n <b>ᴜᴘʟᴏᴀᴅɪɴɢ </b> {2}%\n".format(
             ''.join(["▰" for i in range(math.floor(percentage / 5))]),
             ''.join(["▱" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))──〄\n│\n╰─["[{0}{1}] \n <b>]─〄
+            round(percentage, 2))──〄
 
         tmp = progress + "╭──────〄\n│\n├<b>📤 𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍:</b>{0}│\n├<b>📁 𝐓𝐨𝐭𝐚𝐥 𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞:</b> {1}│\n├<b>🚀𝐒𝐩𝐞𝐞𝐝:</b> {2}/s│\n├<b>⏱️ ᴛɪᴍᴇ ʟᴇғᴛ :</b> {3}\n".format(
             humanbytes(current),
